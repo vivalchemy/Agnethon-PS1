9229a8bbaabb039cee49420955c34384c49e0eac
RonitNaik122 <ronitnaik122@gmail.com>
2024-02-24 21:44:03 +0530

    changes-> choose file+ , dark-theme icon

--- js/calendar.js
+++ js/calendar.js
@@ -454,3 +454,32 @@ function convertTime(time) {
   time = timeHour + ":" + timeMin + " " + timeFormat;
   return time;
 }
+
+
+var inputs = document.querySelectorAll(".file-input")
+
+for (var i = 0, len = inputs.length; i < len; i++) {
+customInput(inputs[i])
+}
+
+function customInput (el) {
+const fileInput = el.querySelector('[type="file"]')
+const label = el.querySelector('[data-js-label]')
+
+fileInput.onchange =
+fileInput.onmouseout = function () {
+if (!fileInput.value) return
+
+var value = fileInput.value.replace(/^.*[\\\/]/,)
+el.className +=  '-chosen'
+label.innerText = value
+}
+}
+
+
+const toggle = document.getElementById("toggleDark");
+const body = document.querySelector("body");
+
+toggle.addEventListener('click', function(){
+  this.classList.toggle("bi-moon");
+})
